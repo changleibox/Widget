b749fad859fa260822e094d6753c56df628ec6b8
Box <changleibox@sina.com>
2017-06-28 00:15:36 +0800

    增加适配器

---
+++ TableView/src/main/java/com/box/me/widget/adapter/TableAdapter.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2017 CHANGLEI. All rights reserved.
+ */
+
+package com.box.me.widget.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.box.me.widget.adapter.viewholder.TableViewHolder;
+
+/**
+ * Created by Box on 2017/6/28.
+ * <p>
+ * 适配器
+ */
+
+public class TableAdapter extends RecyclerView.Adapter<TableViewHolder> {
+
+    @Override
+    public TableViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(TableViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ TableView/src/main/java/com/box/me/widget/adapter/viewholder/TableViewHolder.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2017 CHANGLEI. All rights reserved.
+ */
+
+package com.box.me.widget.adapter.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Box on 2017/6/28.
+ * <p>
+ * ViewHolder
+ */
+
+public class TableViewHolder extends RecyclerView.ViewHolder {
+
+    public TableViewHolder(View itemView) {
+        super(itemView);
+    }
+}
